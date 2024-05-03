1f0e2d173d2962181f52cd2b22da51aa8768fffb
Dhruv <dhruvdm02@gmail.com>
2024-05-03 23:57:28 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
-import React, { Fragment, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-let counter=4;
 function App() {
-  
-  const [todos, setToDos] = useState([{
-    id:1,
-    title:"work1",
-    description:"done1"
-  },{
-    id:2,
-    title:"work2",
-    description:"done2"
-  },{
-    id:3,
-    title:"work3",
-    description:"done3"
-  }
-]);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "work1",
+      description: "done1",
+      completed: false,
+    },
+  ]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos").then(async function (res) {
+        const json = await res.json();
+        console.log(json.todos);
+        setTodos(json.todos);
+      })
+    }, 10000)
+  }, []);
 
   return (
     <div>
-      <button onClick={addToDo}>Add a todo</button>
-      {todos.map(todo => <ToDo title={todo.title} description={todo.description} key={todo.id} />)}
+      {todos.map((todo) => (
+        <ToDo title={todo.title} description={todo.description} />
+      ))}
     </div>
   );
-  
-  function addToDo(){
-    setToDos([...todos,{
-      id:counter++,
-      title:Math.random(),
-      description:Math.random()
-    }])
-  };
-  
 }
 
-function ToDo({title,description}){
-  return <div>
-    <h1>{title}</h1>
-    <h4>{description}</h4>
-  </div>
+function ToDo({ title, description }) {
+  return (
+    <div>
+      <h1>Today's topic is UseEffect</h1>
+      <h5>
+        Write a todo application by fetching data from an API and wrap it inside
+        a useEffect Hook
+      </h5>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 }
 
 export default App;
