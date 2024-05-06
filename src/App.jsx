81217902c48cfaee424e3096225c49026aba00a9
Dhruv <dhruvdm02@gmail.com>
2024-05-07 00:00:13 +0530

    react memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "work1",
-      description: "done1",
-      completed: false,
-    },
-  ]);
-
-  useEffect(() => {
-    setInterval(() => {
-      fetch("https://sum-server.100xdevs.com/todos").then(async function (res) {
-        const json = await res.json();
-        console.log(json.todos);
-        setTodos(json.todos);
-      })
-    }, 10000)
-  }, []);
-
+  const [inputValue, setInputValue] = useState(0);
+  const [counter,setCounter] = useState(0);
+  let sum=0;
+  function calcSum(inputVal){
+    for(let i=0;i<=inputVal;i++){
+      sum= sum+i;
+    }
+    return sum;
+  }
   return (
-    <div>
-      {todos.map((todo) => (
-        <ToDo title={todo.title} description={todo.description} />
-      ))}
-    </div>
-  );
-}
-
-function ToDo({ title, description }) {
-  return (
-    <div>
-      <h1>Today's topic is UseEffect</h1>
-      <h5>
-        Write a todo application by fetching data from an API and wrap it inside
-        a useEffect Hook
-      </h5>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
-  );
+  <div>
+    <input type="text" name="inputVal" onChange={(e)=> setInputValue(e.target.value)}/>
+    <br /><br />
+    Sum is: {calcSum(inputValue)}
+    <br /><br />
+    <button onClick={() => {
+      setCounter(counter+1)
+    }}>Count is {counter}</button>
+  </div>
+    
+  )
 }
 
 export default App;
